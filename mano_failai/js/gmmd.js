e4a7617717cbff807f7599c52ffa8821302dd5c5
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-07-04 18:24:44 +0300

    JS intro

---
+++ mano_failai/js/gmmd.js
@@ -0,0 +1,7 @@
+let metai = 1507;
+let menuo = 7;
+let diena = 18;
+
+let atsakymas = metai % 10 + menuo % 10 + diena % 10;
+
+console.log(atsakymas);
---
+++ mano_failai/js/kodas.js
@@ -0,0 +1,10 @@
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z);
+
+
+let b = "Tautvydas Dulskis";
+let c = "Mano vardas yra";
+
+console.log(c + ' : ' + b);
