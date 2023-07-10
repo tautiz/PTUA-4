7d4a7f2f866b48a0025d22d1cf45162ff56a2b51
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-07-10 20:11:25 +0300

    JS DOM

---
+++ mano_failai/js/DOM_Manipuliacijos.js
@@ -0,0 +1,7 @@
+const manoVardas = "<h1>Vardenis</h1><span>Mano vardas yra Vardenis</span>";
+
+// Pasirinkti elementa
+const manoVardasDOM = document.getElementById("manoVardas");
+
+// Pakeisti elemento turini
+manoVardasDOM.innerHTML = manoVardas;
