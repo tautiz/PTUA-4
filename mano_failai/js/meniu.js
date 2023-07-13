a947a0c9768e6e7471cef78eb759ebccccfb037e
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-07-13 19:28:20 +0300

    Refactoring Meniu generation with document.createElement()

--- mano_failai/js/meniu.js
+++ mano_failai/js/meniu.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
             pavadinimas: 'HOME',
             nuoroda: 'index.html',
             klases: 'meniu_item',
+            id: 'home'
         },
         {
             pavadinimas: 'About',
@@ -23,17 +24,35 @@ document.addEventListener('DOMContentLoaded', function() {
     ];
 
     let busimoMeniuDOMElementai = document.querySelectorAll('.meniu');
-    let meniuHTMLas = '';
+    // let meniuHTMLas = '';
 
     for (let meniuElementoDuomenys of meniuSarasas) {
-        let naujasMeniuElementas = '<a href="' + meniuElementoDuomenys.nuoroda +
-            '" class="' + meniuElementoDuomenys.klases + '">' +
-            meniuElementoDuomenys.pavadinimas + '</a>';
+        // Senas KODAS
+        // let naujasMeniuElementas = '<a href="' + meniuElementoDuomenys.nuoroda +
+        //     '" class="' + meniuElementoDuomenys.klases + '">' +
+        //     meniuElementoDuomenys.pavadinimas + '</a>';
         // REZULTATAS: <a href="index.html" class="meniu_item">Pagrindinis</a>
-        meniuHTMLas += naujasMeniuElementas;
+        // meniuHTMLas += naujasMeniuElementas;
+        //
+        // Naujas KODAS
+        const naujasMeniuElementas = document.createElement('a');
+        // REZULTATAS: <a></a>
+        naujasMeniuElementas.href = meniuElementoDuomenys.nuoroda;
+        // REZULTATAS: <a href="index.html"></a>
+        naujasMeniuElementas.className = meniuElementoDuomenys.klases;
+        // REZULTATAS: <a href="index.html" class="meniu_item"></a>
+        naujasMeniuElementas.textContent = meniuElementoDuomenys.pavadinimas;
+        // REZULTATAS: <a href="index.html" class="meniu_item">Pagrindinis</a>
+
+        // Sekantis ciklas iteruoja per visus meniu elementus ir kiekvienam
+        // prideda nauja meniu elementa.
+        // Si cikla naudojame tik del to kad TURIME 2 MENIU (Desktop ir Mobile)
+        for (let meniuDOMElementas of busimoMeniuDOMElementai) {
+            meniuDOMElementas.append(naujasMeniuElementas.cloneNode(true));
+        }
+        // Jei notime dėti tik į meniu elementa, tai reikia naudoti
+        // const meniuDOMElementas = document.querySelector('.desktop .meniu');
+        // meniuDOMElementas.appendChild(naujasMeniuElementas.cloneNode(true));
     }
 
-    for (let meniuDOMElementas of busimoMeniuDOMElementai) {
-        meniuDOMElementas.innerHTML = meniuHTMLas;
-    }
 });
