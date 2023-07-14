f6b0c5a58edd773f9c19154d53acf683863ef6d4
Tautvydas Dulskis <tautvydas@dulskis.eu>
2023-07-14 18:46:54 +0300

    Adapting Gallery generation by JSON data

---
+++ mano_failai/js/gallery.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const galleryData = [
+        {
+            title: 'Darbas 1',
+            img_url: 'https://picsum.photos/200/300?random=1',
+            tags: ['js', 'html', 'css', 'python'],
+        },
+        {
+            title: 'Darbas 2',
+            img_url: 'https://picsum.photos/200/300?random=2',
+            tags: ['js', 'python'],
+        },
+        {
+            title: 'Darbas 3',
+            img_url: 'https://picsum.photos/200/300?random=3',
+            tags: ['python'],
+        }
+    ];
+
+    let galleryDOMelement = document.querySelector('#work_gallery');
+
+    for (let galleryElementData of galleryData) {
+        const div = document.createElement('div');
+        const img = document.createElement('img');
+        const span = document.createElement('span');
+
+        img.src = galleryElementData.img_url;
+        img.alt = galleryElementData.title;
+        span.innerText = galleryElementData.title;
+
+        div.append(img);
+        div.append(span);
+
+        galleryDOMelement.append(div);
+    }
+});
